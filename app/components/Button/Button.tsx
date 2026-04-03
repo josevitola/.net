e96cbf3e4abfd17b939642824b9f82db1e8948d2
José Vitola <jdnv.123@gmail.com>
2026-04-03 17:58:42 -0500

    Mejorar layout

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,15 +1,16 @@
-type ButtonProps = {
+type LinkButtonProps = React.HTMLAttributes<HTMLAnchorElement> & {
   href: string;
   children?: React.ReactNode;
 }
 
-export function Button({ href, children }: Readonly<ButtonProps>) {
+export function LinkButton({ href, children, className, ...props }: Readonly<LinkButtonProps>) {
   return (
     <a
-      className={`font-serif flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5`}
+      className={`font-serif flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5 ${className ?? ""}`}
       href={href}
       target="_blank"
       rel="noopener noreferrer"
+      {...props}
     >
       {children}
     </a>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo } from "react";
 import { Eye } from "./classes";
-import { Button } from "./components";
+import { LinkButton } from "./components";
 import { EyesCanvas } from "./components/EyesCanvas";
 import { useViewport } from "./hooks/client";
 import { getDefaultEyes } from "./utils/canvas";
@@ -17,13 +17,10 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="font-serif max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Camaleones</h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">Nuevo disco. Julio de 2026.</p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button href="/projects/camaleones">Saber más</Button>
+        <div className="w-full flex flex-col items-center gap-4 text-center sm:items-start sm:text-left">
+          <h1 className="font-serif text-7xl mx-auto font-semibold leading-10 tracking-widest text-black dark:text-zinc-50">camaleones</h1>
+          <p className="max-w-md text-lg leading-8 mx-auto text-zinc-600 dark:text-zinc-400">Nuevo disco. Julio de 2026.</p>
+          <LinkButton href="/projects/camaleones" className="mx-auto">Saber más</LinkButton>
         </div>
       </main>
       <EyesCanvas className="absolute -z-10 top-0 left-0" eyesById={eyes} width={width} height={height} />
