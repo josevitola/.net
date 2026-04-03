1cc5a1c4a5c70e1759c1c08262f274d0237fb69e
José Vitola <jdnv.123@gmail.com>
2026-04-03 18:06:27 -0500

    Rename to LinkButton

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className="w-full flex flex-col items-center gap-4 text-center sm:items-start sm:text-left">
+        <div className="absolute left-0 top-2/3 w-full flex flex-col items-center gap-4 text-center sm:items-start sm:text-left">
           <h1 className="font-serif text-7xl mx-auto font-semibold leading-10 tracking-widest text-black dark:text-zinc-50">camaleones</h1>
           <p className="max-w-md text-lg leading-8 mx-auto text-zinc-600 dark:text-zinc-400">Nuevo disco. Julio de 2026.</p>
           <LinkButton href="/projects/camaleones" className="mx-auto">Saber más</LinkButton>
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,3 @@
-export * from './Button';
+export * from './LinkButton';
 export * from './Canvas';
 export * from './Navbar';
