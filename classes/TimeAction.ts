3995985186d0676fcacc4f068308b110fe102dea
José Vitola <jdnv.123@gmail.com>
2026-04-11 19:51:29 -0500

    Avoid restarting action unnecessarily

--- classes/TimeAction.ts
+++ classes/TimeAction.ts
@@ -1,7 +1,8 @@
 export class TimeAction {
   duration: number = 0;
+  isExecuting: boolean = false;
   private _framesElapsed: number = -1;
-  private _action: (framesElapsed: number) => void
+  private _action: (framesElapsed: number) => void;
 
   constructor(action: (framesElapsed: number) => void) {
     this._action = action;
@@ -18,12 +19,15 @@ export class TimeAction {
   }
 
   start(durationInFrames: number) {
+    if (this.isExecuting) return;
     this._framesElapsed = 0;
     this.duration = durationInFrames;
+    this.isExecuting = true;
   }
 
   stop() {
     this._framesElapsed = -1;
     this.duration = 0;
+    this.isExecuting = false;
   }
 }
